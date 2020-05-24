0fad1be55cbb125e1f07bf90f1bc37fb2dbf5a87
kouki kitamura <kitamura3116@gmail.com>
2020-05-24 19:11:49 +0900

    Create view component to adujest the fine design

---
+++ src/components/internal/view.tsx
@@ -0,0 +1,49 @@
+import React, { FC, HTMLAttributes } from 'react'
+import styled from 'styled-components'
+import {
+  space,
+  SpaceProps,
+  color,
+  ColorProps,
+  typography,
+  TypographyProps,
+  layout,
+  LayoutProps,
+  flexbox,
+  FlexboxProps,
+  grid,
+  GridProps,
+  background,
+  BackgroundProps,
+  borders,
+  BordersProps,
+  position,
+  PositionProps,
+} from 'styled-system'
+
+type StyledSystemProps =
+SpaceProps &
+ColorProps &
+TypographyProps &
+LayoutProps &
+FlexboxProps &
+GridProps &
+BackgroundProps &
+BordersProps &
+PositionProps
+
+const styledSystemProps = [
+  typography,
+  space,
+  layout,
+  flexbox,
+  color,
+  position,
+  grid,
+  background,
+  borders,
+]
+
+export type ViewProps = StyledSystemProps & HTMLAttributes<HTMLDivElement>
+
+export const View = styled('div')<StyledSystemProps>({}, styledSystemProps)
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 import styles from './styles.module.css'
 
 interface Props {
@@ -6,5 +6,6 @@ interface Props {
 }
 
 export const ExampleComponent = ({ text }: Props) => {
+
   return <div className={styles.test}>Example Component: {text}</div>
 }
